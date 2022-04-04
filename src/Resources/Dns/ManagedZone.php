23300511f2c86da743c1f333163271a3e3171753
Dillon Wheeler <dwheeler@gitlab.com>
2022-04-04 12:27:36 -0400

    Update src/Resources/Dns/ManagedZone.php to remove deprecated array verification

--- src/Resources/Dns/ManagedZone.php
+++ src/Resources/Dns/ManagedZone.php
@@ -17,9 +17,6 @@ class ManagedZone extends BaseClient
         $this->managedZoneModel = new ManagedZoneModel();
     }
 
-    const MANAGED_ZONE_OPTIONAL_BODY = ['name', 'visibility', 'dnsName', 'dnssecConfig.state',
-        'cloudLoggingConfig.enableLogging'];
-
     public function get(string $zone_name, $optional_request_data = []): object|string
     {
         return BaseClient::getRequest('/' . $this->project_id . '/managedZones/' . $zone_name, $optional_request_data);
@@ -45,29 +42,5 @@ class ManagedZone extends BaseClient
     {
         return BaseClient::patchRequest('/' . $this->project_id .
             '/managedZones/' . $managed_zone, $request_data);
-    }
-
-    protected function verifyManagedZoneOptionalData(array $managed_zone){
-        foreach (self::MANAGED_ZONE_OPTIONAL_BODY as $parameter) {
-            if (!array_key_exists($parameter, $managed_zone)) {
-                $error_message = 'The ' . $parameter . ' is missing from the ' .
-                    'provided `managed_zone` array. Please ensure that the ' .
-                    '`managed_zone` includes the following values: [' .
-                    implode(', ', self::MANAGED_ZONE_OPTIONAL_BODY) . ']';
-
-                Log::stack((array) config('glamstack-google.auth.log_channels'))
-                    ->critical(
-                        $error_message,
-                        [
-                            'event_type' => 'managed-zone-set-required-parameter-missing',
-                            'class' => get_class(),
-                            'status_code' => '501',
-                            'message' => $error_message,
-                            'missing_parameter' => $parameter
-                        ]
-                    );
-                throw ManagedZoneException::create();
-            }
-        }
     }
 }
