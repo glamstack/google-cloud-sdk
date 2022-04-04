c1972bdf86cd88e62475eef06e57d83b011e01fe
Dillon Wheeler <dwheeler@gitlab.com>
2022-04-04 12:28:57 -0400

    Update tests/Unit/Dns/RecordSetTest.php to include input validation test

--- tests/Unit/Dns/RecordSetTest.php
+++ tests/Unit/Dns/RecordSetTest.php
@@ -28,10 +28,26 @@ it('does not meet the record requirements', function () {
         'log_channels' => ['single'],
         'project_id' => 'dwheeler-277df745'
     ]);
-    $recordSet = $client->dns()->recordSet()->create('testing-zone', [
+    $client->dns()->recordSet()->create('testing-zone', [
         'testing' => 'this should not work'
     ]);
-})->throws(RecordSetException::class, 'The Record Set is missing a required parameter');
+})->throws(UndefinedOptionsException::class);
+
+it('does not have the required type for rrdatas', function() {
+    $client = new Glamstack\GoogleCloud\ApiClient(null, [
+        'api_scopes' => ['https://www.googleapis.com/auth/ndev.clouddns.readwrite'],
+        'email' => 'dwheeler@gitlab.com',
+        'file_path' => 'storage/keys/glamstack-google/gcp_project_1.json',
+        'log_channels' => ['single'],
+        'project_id' => 'dwheeler-277df745'
+    ]);
+    $client->dns()->recordSet()->create('testing-zone', [
+        'name' => 'TestingSet',
+        'ttl' => 300,
+        'type' => 'CNAME',
+        'rrdatas' => 'mail.testingzone.example.com.'
+    ]);
+})->throws(InvalidOptionsException::class);
 
 it('can create a recordSet', function() {
     $testing = new Glamstack\GoogleCloud\ApiClient('gcp_project_1');
