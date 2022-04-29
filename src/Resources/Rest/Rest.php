4af2d36595465059a736171d1c978a1c8c0df64c
Dillon Wheeler <dwheeler@gitlab.com>
2022-04-29 14:50:22 -0400

    Update src/Resources/Rest/Rest.php to support specific usage pattern

--- src/Resources/Rest/Rest.php
+++ src/Resources/Rest/Rest.php
@@ -10,4 +10,34 @@ class Rest extends ApiClient
     {
         return new Method($this);
     }
+
+    public function get(string $uri, array $request_data = []): object|string
+    {
+        $method = new Method($this);
+        return $method->get($uri, $request_data);
+    }
+
+    public function post(string $uri,array $request_data = []): object|string
+    {
+        $method = new Method($this);
+        return $method->post($uri, $request_data);
+    }
+
+    public function patch(string $uri, array $request_data = []): object|string
+    {
+        $method = new Method($this);
+        return $method->patch($uri, $request_data);
+    }
+
+    public function put(string $uri, array $request_data = []): object|string
+    {
+        $method = new Method($this);
+        return $method->put($uri, $request_data);
+    }
+
+    public function delete(string $uri, array $request_data = []): object|string
+    {
+        $method = new Method($this);
+        return $method->delete($uri, $request_data);
+    }
 }
