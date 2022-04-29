30d4f6a6ec79241449c23875fa5dbd4691945f8e
Dillon Wheeler <dwheeler@gitlab.com>
2022-04-29 14:58:07 -0400

    Add tests/Unit/Rest/MethodTest.php

---
+++ tests/Unit/Rest/MethodTest.php
@@ -0,0 +1,26 @@
+<?php
+
+it('can use get request to get managed zones', function(){
+   $client = new Glamstack\GoogleCloud\ApiClient('test');
+   $response = $client->rest()->get('/' . env('GOOGLE_CLOUD_TEST_PROJECT_ID') . '/managedZones', []);
+   expect($response->status->code)->toBe(200);
+});
+
+it('can use post request to create a new managed zone', function(){
+    $client = new Glamstack\GoogleCloud\ApiClient('test');
+    $request_data = [
+        'name' => 'testing-zone-3',
+        'dns_name' => 'testing-zone-3.example.com.',
+        'visibility' => 'private',
+        'dnssec_config_state' => 'off',
+        'description' => 'Testing zone 3 by SDK',
+    ];
+    $response = $client->rest()->post('/' . env('GOOGLE_CLOUD_TEST_PROJECT_ID') . '/managedZones', $request_data);
+    expect($response->status->code)->toBe(200);
+});
+
+it('can use delete request to delete a managed zone', function(){
+    $client = new Glamstack\GoogleCloud\ApiClient('test');
+    $response = $client->rest()->delete('/' . env('GOOGLE_CLOUD_TEST_PROJECT_ID') . '/managedZones/testing-zone-3');
+    expect($response->status->successful)->toBeTrue();
+});
