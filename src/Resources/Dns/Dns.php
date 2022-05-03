0c2b7d1e24cea3ec15ee14f164088701d12fad14
Dillon Wheeler <dwheeler@gitlab.com>
2022-05-03 10:03:13 -0400

    Update src/Resources/Dns/Dns.php to add documentation

--- src/Resources/Dns/Dns.php
+++ src/Resources/Dns/Dns.php
@@ -6,11 +6,21 @@ use Glamstack\GoogleCloud\ApiClient;
 
 class Dns extends ApiClient
 {
+    /**
+     * Creates a RecordSet object
+     *
+     * @return RecordSet
+     */
     public function RecordSet(): RecordSet
     {
         return new RecordSet($this);
     }
 
+    /**
+     * Creates a ManagedZone object
+     *
+     * @return ManagedZone
+     */
     public function ManagedZone(): ManagedZone
     {
         return new ManagedZone($this);
