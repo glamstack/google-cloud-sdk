d7b9244214fe2ca0e5e9c2c93689b503e32a76be
Dillon Wheeler <dwheeler@gitlab.com>
2022-05-05 15:47:47 -0400

    Update tests/Unit/Rest/MethodTest.php to include the `base_url`
    The base_url was originally in the wrong class

--- tests/Unit/Rest/MethodTest.php
+++ tests/Unit/Rest/MethodTest.php
@@ -2,7 +2,7 @@
 
 it('can use get request to get managed zones', function(){
    $client = new Glamstack\GoogleCloud\ApiClient('test');
-   $response = $client->rest()->get('/' . env('GOOGLE_CLOUD_TEST_PROJECT_ID') . '/managedZones', []);
+   $response = $client->rest()->get('https://dns.googleapis.com/dns/v1/projects/' . env('GOOGLE_CLOUD_TEST_PROJECT_ID') . '/managedZones', []);
    expect($response->status->code)->toBe(200);
 });
 
@@ -15,12 +15,12 @@ it('can use post request to create a new managed zone', function(){
         'dnssec_config_state' => 'off',
         'description' => 'Testing zone 3 by SDK',
     ];
-    $response = $client->rest()->post('/' . env('GOOGLE_CLOUD_TEST_PROJECT_ID') . '/managedZones', $request_data);
+    $response = $client->rest()->post('https://dns.googleapis.com/dns/v1/projects/' . env('GOOGLE_CLOUD_TEST_PROJECT_ID') . '/managedZones', $request_data);
     expect($response->status->code)->toBe(200);
 });
 
 it('can use delete request to delete a managed zone', function(){
     $client = new Glamstack\GoogleCloud\ApiClient('test');
-    $response = $client->rest()->delete('/' . env('GOOGLE_CLOUD_TEST_PROJECT_ID') . '/managedZones/testing-zone-3');
+    $response = $client->rest()->delete('https://dns.googleapis.com/dns/v1/projects/' . env('GOOGLE_CLOUD_TEST_PROJECT_ID') . '/managedZones/testing-zone-3');
     expect($response->status->successful)->toBeTrue();
 });
