2a4638ca7a6e4ce356e8252956b375440cbf7239
Dillon Wheeler <dwheeler@gitlab.com>
2022-05-11 11:04:49 -0400

    Update src/ApiClient.php documentation and uncommented `setRequestHeaders` logic

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -78,14 +78,13 @@ class ApiClient
      *
      * If no connection_key is provided, the `GOOGLE_AUTH_DEFAULT_CONNECTION`
      * variable in `.env` is used. If the `.env` variable is not set, the value
-     * is defined in `config/glamstack-google-cloud.php` and is set to `workspace` if
-     * not defined since the configuration is shared with the Google Workspace
-     * SDK. This can be overridden when initializing the SDK with a different
-     * connection key which is passed into this function to set the class
-     * variable to the provided key.
+     * is defined in `config/glamstack-google-cloud.php` and is set to `test` if
+     * not defined. This can be overridden when initializing the SDK with a 
+     * different connection key which is passed into this function to set the 
+     * class variable to the provided key.
      *
-     * @param ?string $connection_key (Optional) The connection key to use from
-     * the configuration file.
+     * @param ?string $connection_key (Optional) 
+     *      The connection key to use from the configuration file.
      *
      * @return void
      */
@@ -101,7 +100,7 @@ class ApiClient
     }
 
     /**
-     * Set the request headers for the GitLab API request
+     * Set the request headers for the Google Cloud API request
      *
      * @return void
      */
@@ -112,22 +111,23 @@ class ApiClient
         $php = 'php/'.phpversion();
 
         // Decode the composer.lock file
-//        $composer_lock_json = json_decode(
-//            (string) file_get_contents(base_path('composer.lock')),
-//            true
-//        );
+        $composer_lock_json = json_decode(
+            (string) file_get_contents(base_path('composer.lock')),
+            true
+        );
 
         // Use Laravel collection to search for the package. We will use the
         // array to get the package name (in case it changes with a fork) and
         // return the version key. For production, this will show a release
         // number. In development, this will show the branch name.
         /** @phpstan-ignore-next-line */
-//        $composer_package = collect($composer_lock_json['packages'])
-//            ->where('name', 'glamstack/google-cloud-dns-sdk')
-//            ->first();
+        $composer_package = collect($composer_lock_json['packages'])
+            ->where('name', 'glamstack/google-cloud-sdk')
+            ->first();
+
         /** @phpstan-ignore-next-line */
-//        $package = $composer_package['name'].'/'.$composer_package['version'];
-        $package = 'glamstack-google-cloud/1';
+        $package = $composer_package['name'].'/'.$composer_package['version'];
+
         // Define request headers
         $this->request_headers = [
             'User-Agent' => $package.' '.$laravel.' '.$php
