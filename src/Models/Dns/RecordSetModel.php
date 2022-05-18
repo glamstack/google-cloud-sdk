5f34dfb45d69a3610331907b5224e689bbfebcad
Dillon Wheeler <dwheeler@gitlab.com>
2022-05-18 16:41:46 -0400

    Update src/Models/Dns/RecordSetModel.php to have remaining validation and to throw exceptions

--- src/Models/Dns/RecordSetModel.php
+++ src/Models/Dns/RecordSetModel.php
@@ -2,11 +2,12 @@
 
 namespace Glamstack\GoogleCloud\Models\Dns;
 
+use Exception;
 use Illuminate\Support\Facades\Validator;
 
 class RecordSetModel
 {
-    public function get(array $options = []){
+    public function get(array $options){
         $validator = Validator::make($options,
             [
                 'managed_zone' => 'required|string',
@@ -20,7 +21,7 @@ class RecordSetModel
         );
 
         if ($validator->fails()) {
-            dd($validator->messages()->first());
+            throw new Exception($validator->messages()->first());
         }
 
         $path_parameters = (object) [
@@ -42,7 +43,10 @@ class RecordSetModel
         return $return_value;
     }
 
-    public function create(array $options = [])
+    /**
+     * @throws Exception
+     */
+    public function create(array $options)
     {
         $default_ttl = 300;
 
@@ -60,7 +64,7 @@ class RecordSetModel
         ]);
 
         if ($validator->fails()) {
-            dd($validator->messages()->first());
+            throw new Exception($validator->messages()->first());
         }
 
         $path_parameters = (object) [
@@ -74,6 +78,72 @@ class RecordSetModel
 
         $return_value = (object) [
             'path_parameters' => $path_parameters,
+            'request_data' => $request_data
+        ];
+
+        return $return_value;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function list($options)
+    {
+        $validator = Validator::make($options,
+            [
+                'managed_zone' => 'required|string',
+                'project_id' => 'required|string',
+            ],
+        );
+
+        if ($validator->fails()) {
+            throw new Exception($validator->messages()->first());
+        }
+
+        $path_parameters = (object) [
+            'managed_zone' => $options['managed_zone'],
+            'project_id' => $options['project_id']
+        ];
+
+        $request_data = $options;
+        unset($request_data['managed_zone'], $request_data['project_id']);
+
+        return (object) [
+            'path_parameters' => $path_parameters,
+            'request_data' => $request_data
+        ];
+    }
+
+    public function delete(array $options){
+        $validator = Validator::make($options,
+            [
+                'managed_zone' => 'required|string',
+                'project_id' => 'required|string',
+                'name' => 'required|string',
+                'type' => 'required|string|in:CNAME,A,AAAA',
+            ],
+            [
+                'type.in' => 'Available types are CNAME,A,AAAA'
+            ]
+        );
+
+        if ($validator->fails()) {
+            throw new Exception($validator->messages()->first());
+        }
+
+        $path_parameters = (object) [
+            'managed_zone' => $options['managed_zone'],
+            'project_id' => $options['project_id'],
+            'name' => $options['name'],
+            'type' => $options['type']
+        ];
+
+        $request_data = $options;
+        unset($request_data['managed_zone'], $request_data['project_id'], $request_data['name'], $request_data['type']);
+
+
+        $return_value = (object) [
+            'path_parameter' => $path_parameters,
             'request_data' => $request_data
         ];
 
